e6e22ccd09ad2403331279732f92de4f8265ca20
djw <13292353138@163.com>
2019-05-25 00:59:26 +0800

    add 第一次创建应用

---
+++ src/main/java/com/djw/nodemcu/NodemcuApplication.java
@@ -0,0 +1,13 @@
+package com.djw.nodemcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodemcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodemcuApplication.class, args);
+	}
+
+}
